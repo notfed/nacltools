b0f884b361211e0b60a9a34973a181cdf374f851
Jay Sullivan <jay@petio.org>
2010-10-04 19:55:39 -0400

    clean up crypto-box's argument handling and manpage

--- crypto-box.c
+++ crypto-box.c
@@ -19,7 +19,6 @@ static stralloc m = {0};
 static stralloc c = {0};
 static stralloc n = {0};
 #define FATAL "crypto-box: error: "
-
 void show(const char *desc, stralloc *s)
 { 
   buffer_puts(buffer_2,desc);
@@ -30,13 +29,15 @@ void show(const char *desc, stralloc *s)
 int main(int argc, char * argv[])
 {
     int r;
+    const char* pk0;
+    const char* sk0;
 
     /* Check args */
-    if(argc!=3) {
-        if( (argv[1] = env_get("NACL_PUBLICKEY_FILE"))==0 
-        ||  (argv[2] = env_get("NACL_SECRETKEY_FILE"))==0 )
-        strerr_die1x(100,"usage: crypto-box [ publickeyfile secretkeyfile ]");
-    }
+    pk0 = ((argc<=1) ? env_get("NACL_PUBLICKEY_FILE") : argv[1]);
+    sk0 = ((argc<=2) ? env_get("NACL_SECRETKEY_FILE") : argv[2]);
+
+    if(pk0==0 || sk0==0) 
+        strerr_die1x(100,"crypto-box: usage: crypto-box [ publickeyfile ] [ secretkeyfile ]");
 
     /* Read public key */
     if(openreadclose(argv[1],&pk,crypto_box_PUBLICKEYBYTES)<=0)
